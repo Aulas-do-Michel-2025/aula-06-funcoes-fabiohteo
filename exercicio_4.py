9280d296bbd5d36501f150d61d2fdd24eace63eb
fabiohteo <fhaach@gmail.com>
2025-07-11 00:40:30 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -17,8 +17,9 @@ def verificar_se_eh_primo(numero):
         return True
     return False
 
-
 def filtrar_lista_por_numeros_primos(lista):
     lista_filtrada = []
-    # Completar funcao aqui!
+    for numero in lista:
+        if verificar_se_eh_primo(numero):
+            lista_filtrada.append(numero)
     return lista_filtrada
